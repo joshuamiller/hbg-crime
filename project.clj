9fc56fbcc09ae38d3f0cb9d0ac9f9dff2a89c94a
Joshua Miller <elefantstn@gmail.com>
2013-09-20 18:15:48 -0400

    Scrape report PDF locations; export as JSON or CSV.

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.pdfbox/pdfbox "1.8.2"]])
+                 [org.apache.pdfbox/pdfbox "1.8.2"]
+                 [org.clojars.scsibug/feedparser-clj "0.4.0"]
+                 [enlive/enlive "1.1.1"]
+                 [cheshire "5.0.2"]
+                 [clojure-csv/clojure-csv "2.0.0-alpha1"]
+])
--- src/hbg_crime/core.clj
+++ src/hbg_crime/core.clj
@@ -1,12 +1,33 @@
 (ns hbg-crime.core
   (:import [org.apache.pdfbox.pdmodel PDDocument]
            [org.apache.pdfbox.util PDFTextStripper]
-           [java.io File])
-  (:require [clojure.string :as str]))
+           [java.io File StringWriter]
+           [java.net URL])
+  (:require [clojure.string :as str]
+            [feedparser-clj.core :as feed]
+            [net.cgrand.enlive-html :as html]
+            [cheshire.core :as json]
+            [clojure-csv.core :as csv]))
+
+(defn police-blog-feed
+  []
+  (feed/parse-feed "http://harrisburgpa.gov/feed/"))
+
+(defn pdf-link-for-url
+  [url]
+  (let [source (-> url URL. html/html-resource)]
+    (-> (html/select source [:div.post_text :p :a]) first :attrs :href)))
+
+(defn current-crime-report-links
+  []
+  (->> (:entries (police-blog-feed))
+       (filter #(re-find #"Crime Report" (:title %)))
+       (map :link)
+       (map pdf-link-for-url)))
 
 (defn text-of-pdf
-  [src]
-  (with-open [pd (PDDocument/load (File. src))]
+  [url]
+  (with-open [pd (PDDocument/load (URL. url))]
     (let [stripper (PDFTextStripper.)]
       (.getText stripper pd))))
 
@@ -26,3 +47,25 @@
         lines (str/split text #"\n")
         parsed-lines (map parse-line lines)]
     (filter identity parsed-lines)))
+
+(defn all-current-reports
+  []
+  (->> (current-crime-report-links)
+       (map results-of-file)
+       (flatten)
+       (distinct)))
+
+(defn reports-as-json
+  [reports]
+  (json/encode reports))
+
+(defn reports-as-csv
+  [reports]
+  (->> reports
+       (map #(vec [(or (:start-time %) "")
+                   (:end-time %)
+                   (:address %)
+                   (:description %)]))
+       (cons ["Start (if applicable)" "End" "Address" "Description"])
+       (vec)
+       (csv/write-csv)))
