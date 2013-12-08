102b98f039d731be95c573154433bf770c148953
Joshua Miller <elefantstn@gmail.com>
2013-12-08 15:09:32 -0500

    Better destructuring in geometry/crossing-number.

--- src/clj/hbg_crime/geometry.clj
+++ src/clj/hbg_crime/geometry.clj
@@ -6,15 +6,14 @@
 (defn- crossing-number
   "Determine crossing number for given point and segment of a polygon.
    See http://geomalgorithms.com/a03-_inclusion.html"
-  [point [[x1 y1] [x2 y2]]]
-  (let [[px py] point]
-    (if (or (and (<= y1 py) (> y2 py))
-            (and (> y1 py) (<= y2 py)))
-      (let [vt (/ (- py y1) (- y2 y1))]
-        (if (< px (+ x1 (* vt (- x2 x1))))
-          1 0))
-      0)))
-  
+  [[px py] [[x1 y1] [x2 y2]]]
+  (if (or (and (<= y1 py) (> y2 py))
+          (and (> y1 py) (<= y2 py)))
+    (let [vt (/ (- py y1) (- y2 y1))]
+      (if (< px (+ x1 (* vt (- x2 x1))))
+        1 0))
+    0))
+
 (defn inside?
   "Is point inside the given polygon?"
   [point polygon]
